74173cb44515cd8ce16ea1e749e18dfb0362420a
DiegoAPaez <diego.p31@hotmail.com>
2025-07-31 11:02:55 +0200

    Added Tailwind and deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react(), tailwindcss()],
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+        sourcemap: false,
+        emptyOutDir: true,
+    },
+    publicDir: "public",
+    server: {
+        port: 5173,
+        open: true,
+    },
+});
